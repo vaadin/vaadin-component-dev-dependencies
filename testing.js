a4bdf7b5c76c3815fe1e40003466c874cba36ab8
web-padawan <iamkulykov@gmail.com>
2019-11-06 11:16:57 +0200

    Add axe-core, chai-a11y-axe and helpers

---
+++ testing.js
@@ -0,0 +1,13 @@
+import 'chai/chai.js';
+import { chaiA11yAxe } from 'chai-a11y-axe';
+
+window.chai.use(chaiA11yAxe);
+
+export {
+  html,
+  aTimeout,
+  nextFrame,
+  fixture
+} from '@open-wc/testing-helpers/index.js';
+
+export const { expect } = window.chai;
