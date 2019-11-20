d383835adebe5893607c7362866ad6c0ecb4d68e
Serhii Kulykov <iamkulykov@gmail.com>
2019-11-20 14:39:32 +0200

    Add test helpers for keyboard events (#9)
    * Add test helpers for keyboard events
    
    * Add helpers for enter and space keys

---
+++ keys.d.ts
@@ -0,0 +1,119 @@
+export declare function makeKeyboardEvent(
+  key: number,
+  modifiers?: string | string[]
+): Event;
+
+export declare function tabKeyDown(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function tabKeyUp(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function tab(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function arrowDownKeyDown(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function arrowDownKeyUp(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function arrowDown(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function arrowLeftKeyDown(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function arrowLeftKeyUp(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function arrowLeft(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function arrowRightKeyDown(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function arrowRightKeyUp(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function arrowRight(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function arrowUpKeyDown(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function arrowUpKeyUp(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function arrowUp(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function home(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function end(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function enterKeyDown(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function enterKeyUp(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function enter(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function spaceKeyDown(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function spaceUpKeyUp(
+  target: Element,
+  modifiers?: string | string[]
+): void;
+
+export declare function space(
+  target: Element,
+  modifiers?: string | string[]
+): void;
